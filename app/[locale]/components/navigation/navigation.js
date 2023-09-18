859f0841870059df62d895031921aa9cc9715739
OS <scherbakoleg94@gmail.com>
2023-09-19 02:46:10 +0300

    add korean lang support

---
+++ app/[locale]/components/navigation/navigation.js
@@ -0,0 +1,61 @@
+"use client";
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useTranslations, useMessages, useLocale} from "next-intl";
+import Image from 'next/image';
+import LogoImg from '../../assets/logo.png';
+import UsaImg from './assets/usa.png';
+import KoreaImg from './assets/korea.png';
+import styles from './navigation.module.scss';
+import Link from 'next/link';
+
+const Navigation = () => {
+    const locale = useLocale();
+    const t = useTranslations('Navigation');
+
+    const handleLanguageIconClick = (e, lang) => {
+        e.preventDefault();
+        e.stopPropagation();
+        window.location.href = `/${lang}`
+    }
+
+    return <Navbar collapseOnSelect className={styles.navigation} expand="lg" bg="light" variant="light">
+        <Navbar.Brand href="/">
+            <Image src={LogoImg} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="/"><span className={styles.linkText}> {t('main')} </span></Nav.Link>
+                <Nav.Link href={`${locale}/metaverse-introduction`}><span className={styles.linkText}> {t('metaverseIntroduction')} </span></Nav.Link>
+                <Nav.Link href={`${locale}/business-background`}><span className={styles.linkText}> {t('business')} </span></Nav.Link>
+                <Nav.Link href={`${locale}/corp-introduction`}><span className={styles.linkText}> {t('corpIntro')} </span></Nav.Link>
+                <Nav.Link href={`${locale}/roadmap`}><span className={styles.linkText}> {t('roadmap')} </span></Nav.Link>
+                <Nav.Link href={`${locale}/coin-distribution`}><span className={styles.linkText}> {t('coinDistribution')} </span></Nav.Link>
+                <Nav.Link href={`${locale}/partners`}> <span className={styles.linkText}> {t('partners')} </span></Nav.Link>
+                <Nav.Link href='#'>
+                    <span className={styles.linkText}>
+                        <Image src={UsaImg} height={20} onClick={(e) => handleLanguageIconClick(e, 'en')} /> | <Image src={KoreaImg} height={25} onClick={(e) => handleLanguageIconClick(e, 'kr')}  />
+                    </span>
+                </Nav.Link>
+                {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
+                {/*    <NavDropdown.Item href="/">Home</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Item href="/about-us">About us</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Divider />*/}
+                {/*    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>*/}
+                {/*</NavDropdown>*/}
+            </Nav>
+            {/*<Nav>*/}
+            {/*    <Nav.Link href="#deets">More deets</Nav.Link>*/}
+            {/*    <Nav.Link eventKey={2} href="#memes">*/}
+            {/*        Dank memes*/}
+            {/*    </Nav.Link>*/}
+            {/*</Nav>*/}
+        </Navbar.Collapse>
+    </Navbar>
+}
+
+export default Navigation;
--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,13 +1,19 @@
-import {useLocale} from 'next-intl';
+import {NextIntlClientProvider, useLocale, useMessages, useTranslations} from 'next-intl';
 import {notFound} from 'next/navigation';
-import Navigation from "./components/navigation";
+import Navigation from "./components/navigation/navigation";
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from './layout.module.scss';
 
-export default function LocaleLayout({ children, params }) {
-  const locale = useLocale();
+const getMessages = async (locale) => {
+    const messages = (await import(`../../messages/${locale}.json`)).default;
 
-  // Validate that the incoming `locale` parameter is a valid locale
+    return messages;
+}
+
+export default async function LocaleLayout({ children, params }) {
+  const locale = useLocale();
+  const messages = await getMessages(locale);
+
   if (params.locale !== locale) {
     notFound();
   }
@@ -15,15 +21,16 @@ export default function LocaleLayout({ children, params }) {
   return (
       <html lang={locale}>
         <body>
-        <header className={styles.header}>
-            <Navigation locale={locale} />
-        </header>
-        <div className={styles.main}>
-            {children}
-        </div>
+        <NextIntlClientProvider messages={messages} locale={locale} >
+            <header className={styles.header}>
+                <Navigation />
+            </header>
+            <div className={styles.main}>
+                {children}
+            </div>
+        </NextIntlClientProvider>
         <footer className={styles.footer}>
-            <span className={styles.disclaimer}>Disclaimer</span>: Our site offers general info, not professional advice. Cryptocurrency investments are risky and past performance doesn&apos;t predict future outcomes. Research and seek advice before deciding. We might link to third-party content, but accuracy isn&apos;t assured. We&apos;re not responsible for errors or actions based on our info. Laws for cryptocurrencies vary; follow your local rules. Using our site means you accept this disclaimer and its risks. Consult experts for personalized advice. Be cautious with crypto matters.
-            Word Coin reserves the right to modify or update this disclaimer at any time without prior notice.
+            <span className={styles.disclaimer}>{messages.Footer?.highlight}</span>: {messages.Footer?.text}
         </footer>
         </body>
       </html>
